56db0b379ab627705f4f0c4aa2ad132469e9fffe
oleast <oleast@stud.ntnu.no>
2018-10-25 10:39:29 +0200

    Remove redundant Login button from frontpage

--- src/frontpage/index.tsx
+++ src/frontpage/index.tsx
@@ -2,15 +2,14 @@ import React, { Fragment } from 'react';
 import EventsContainer from 'events/components/EventsContainer';
 import Offline from './components/Offline';
 import Articles from './components/Articles';
-import Login from 'authentication/components/Login';
 
 const Frontpage = () => (
   <>
-    <Login />
     <EventsContainer />
     <Articles />
     <Offline />
   </>
+
 );
 
 export default Frontpage;
