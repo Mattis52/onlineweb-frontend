eb79f71370bdafd3fdaf3aee456c76a8f6e19ae9
Tobias Kongsvik <tokongs98@gmail.com>
2018-10-29 23:42:28 +0100

    Proof of work using react-google-captcha

--- src/events/components/DetailView/Registation.tsx
+++ src/events/components/DetailView/Registation.tsx
@@ -2,13 +2,17 @@ import React from 'react';
 import { INewEvent, getEventColor } from '../../models/Event';
 import style from './detail.less';
 import CardHeader from './Card/CardHeader';
-
-const Registration = ({ event_type }: INewEvent) => {
-  const color = getEventColor(event_type)
+interface IProps {
+  event_type: number,
+  on_sign_up: () => void
+}
+const Registration = (props : IProps) => {
+  const color = getEventColor(props.event_type)
   return (
     <div className={style.registration}>
       <div className={style.cardMargin}>
         <CardHeader color={color}>Påmelding</CardHeader>
+        <button onClick={props.on_sign_up}>Meld meg på</button>
         <p></p>
       </div>
     </div>
--- src/events/components/DetailView/index.tsx
+++ src/events/components/DetailView/index.tsx
@@ -7,6 +7,10 @@ import style from './detail.less';
 import InfoBox from './InfoBox';
 import Registration from './Registation';
 import Contact from './Contact';
+import ReCAPTCHA from 'react-google-recaptcha'
+import { throws } from 'assert';
+import { post } from '../../../common/utils/api'
+import axios from 'axios';
 
 export interface IProps {
   eventId: string;
@@ -15,6 +19,7 @@ export interface IProps {
 export interface IState {
   eventId: number;
   event: INewEvent | null;
+  captchaRequested: boolean;
 }
 
 class DetailView extends Component<IProps, IState> {
@@ -23,7 +28,8 @@ class DetailView extends Component<IProps, IState> {
 
     this.state = {
       eventId: parseInt(props.eventId, 10),
-      event: null
+      event: null,
+      captchaRequested: false
     };
   }
 
@@ -33,18 +39,38 @@ class DetailView extends Component<IProps, IState> {
     this.setState({ event });
   }
 
+  private onSignUp() {
+    this.setState({ captchaRequested: true });
+  }
+
+  private onChange(value: string | null) {
+    if (value != null) {
+      const validated = axios.post("http://localhost:8000/api/v1/sign_up/", { "captcha_value": value }).then(o => { console.log(o) });
+    }
+  }
+
   public render() {
     const { event, eventId } = this.state;
     const e = event || mockEvent;
+    let captcha;
+    console.log(this.state.captchaRequested)
+
+    if (this.state.captchaRequested) {
+      captcha = <ReCAPTCHA
+        sitekey="6LfV9jkUAAAAANqYIOgveJ0pOowXvNCcsYzRi7Y_"
+        onChange={this.onChange} />
+    }
+
     return (
       <div className={style.container}>
+        {captcha}
         <div className={style.leftContainer}>
           <ListEvent key={eventId} {...e} />
           <PictureCard key={eventId} {...e} />
-          <InfoBox {...e}/>
+          <InfoBox {...e} />
         </div>
         <div className={style.rightContainer}>
-          <Registration {...e} />
+          <Registration event_type={e.event_type} on_sign_up={this.onSignUp.bind(this)} />
           <Contact {...e} />
         </div>
       </div>
