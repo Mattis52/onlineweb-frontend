28affecace8a9be8164f559f66ba8038018e3186
Tobias Kongvik <tokongs98@gmail.com>
2018-10-31 21:42:17 +0100

    e.what

--- src/events/components/ListView/ListEvent.tsx
+++ src/events/components/ListView/ListEvent.tsx
@@ -3,6 +3,7 @@ import { DateTime } from 'luxon';
 import { INewEvent, getEventColor, getEventType } from '../../models/Event';
 import HostPolygon from './HostPolygon';
 import StatusPolygon from './StatusPolygon';
+import Person from '../../icons/Person'
 import style from './list.less';
 import { Link } from 'react-router-dom';
 
@@ -15,29 +16,31 @@ const ListEvent = ({ title, event_start, attendance_event, event_type, company_e
     <Link to={`/events/${id}`}>
       <div className={style.gridRow}>
         <div className={style.elementGridRow} style={{ left: 0 }}>
-          <HostPolygon color={ eventColor } />
-          { /* Optional chainging, tc39 stage 1 proposal. Not supported by Typescript  */ }
-          <p>{ company_event[0]?.company.name || eventType }</p>
+          <HostPolygon color={eventColor} />
+          { /* Optional chainging, tc39 stage 1 proposal. Not supported by Typescript  */}
+          <p>{company_event[0] ?.company.name || eventType }</p>
         </div>
 
         <div className={style.elementGridRow}>
-          <p style={{ verticalAlign: 'center' }}>{ title }</p>
+          <p style={{ verticalAlign: 'center' }}>{title}</p>
         </div>
         <div className={style.elementGridRow}>
           <p>
-            { /* Optional chainging, tc39 stage 1 proposal. Not supported by Typescript  */ }
-            { attendance_event
-              ? `${attendance_event?.attendees?.length || 0}/${attendance_event.max_capacity}`
+            { /* Optional chainging, tc39 stage 1 proposal. Not supported by Typescript  */}
+            {attendance_event
+              ? `${attendance_event ?.attendees ?.length || 0}/${attendance_event.max_capacity}`
               : 'ALLE'
             }
           </p>
         </div>
         <div className={style.elementGridRow}>
-          <p>{ eventDate }</p>
+          <p>{eventDate}</p>
         </div>
         <div className={style.elementGridRow}>
           <StatusPolygon color="#828282" />
-          <p>{ attendance_event ? 'Y' : 'N' }</p>
+          <div className={style.person}>
+            <Person color="#ffffff" />
+          </div>
         </div>
       </div>
     </Link>
